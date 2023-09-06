04736b4bfdec3732dc222d8ff2f495d4cf8e7c52
Ahanaf Asir <ahanafasir83@gmail.com>
2023-09-06 15:55:25 +0600

    updated with functinalities

---
+++ README.md
@@ -0,0 +1,58 @@
+# JavaScript Number Guessing Game
+
+This is a simple number guessing game implemented in JavaScript, utilizing Document Object Model (DOM) manipulation to create an interactive user experience. The game is designed for players to guess a randomly generated number between 1 and 20.
+
+## Features
+
+- Randomly generated number between 1 and 20 for each game session.
+- Interactive interface with input field, submit button, and feedback messages.
+- Keeps track of the number of attempts.
+- Provides feedback to the player after each guess: "Too low," "Too high," or "Congratulations, you guessed the number!"
+- Option to restart the game for a new round.
+
+## How to Play
+
+1. Open the `index.html` file in your web browser.
+
+2. You will see the game interface, including an input field to enter your guess, a submit button to submit your guess, and feedback messages to guide you.
+
+3. Enter your guess in the input field and click the "Submit" button.
+
+4. The game will provide feedback on your guess. If your guess is too low or too high, it will let you know. If you guessed the number correctly, it will congratulate you and display the number of attempts it took.
+
+5. To start a new round, click the "Restart" button.
+
+## Technologies Used
+
+- HTML: The structure of the game.
+- CSS: Styling the user interface.
+- JavaScript: Implementing the game logic and DOM manipulation.
+
+## Files Included
+
+- `index.html`: The main HTML file containing the game interface.
+- `style.css`: CSS file for styling the game.
+- `queries.css`: CSS file for media queries.
+- `script.js`: JavaScript file containing the game logic and DOM manipulation.
+
+## Game Logic
+
+The game logic is implemented in `script.js` and includes the following steps:
+
+1. Generate a random number between 1 and 20 at the start of each game session.
+
+2. Listen for user input (guess) and validate it to ensure it is a number between 1 and 20.
+
+3. Compare the user's guess with the generated random number.
+
+4. Provide feedback to the user based on their guess (too low, too high, or correct).
+
+5. Keep track of the number of attempts and display it to the user.
+
+6. Allow the user to restart the game for a new round.
+
+## Customization
+
+You can customize the game further by modifying the CSS styles or changing the range of numbers for guessing by editing the JavaScript code in `script.js`.
+
+Feel free to explore and enhance the game as you like!
--- js/script.js
+++ js/script.js
@@ -1,1 +1,69 @@
-"use strict"; // strict mode enabled
+"use strict"; // Enables strict mode for better coding practices
+
+// Generate a random secret number between 1 and 20
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+// Initialize the player's score and highscore
+let score = 20;
+let highscore = 0;
+
+// Function to display messages on the web page
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
+// Event listener for the "Check" button
+document.querySelector(".check").addEventListener("click", function () {
+  // Get the user's guess from the input field
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess); // Log the guess to the console
+
+  // When there is no input (empty or not a number)
+  if (!guess) {
+    displayMessage("⛔️ No number!");
+
+    // When the player wins
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber; // Display the correct number
+
+    document.querySelector("body").style.backgroundColor = "#60b347"; // Change background color
+    document.querySelector(".number").style.width = "30rem"; // Increase the width of the number box
+
+    // Update the highscore if the current score is higher
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    // When the guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // Provide hints and reduce the score if there are remaining attempts
+      displayMessage(guess > secretNumber ? "⬆️ Too high!" : "⬇️ Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      // When the player runs out of attempts
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+// Event listener for the "Again" button (to start a new game)
+document.querySelector(".again").addEventListener("click", function () {
+  // Reset the score and generate a new secret number
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // Reset messages and input field
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  // Reset background color and number box width
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
